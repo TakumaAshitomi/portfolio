cfd1b54f0c0209cd1509da567ae2d07acf0c6ca1
Kemomoy <surumekarasu@gmail.com>
2019-04-15 09:21:17 +0000

    fix action_mailer domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,12 +66,12 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'https://damp-reaches-55435.herokuapp.com' }
-    config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
       :user_name => ENV["SENDGRID_ID"],
       :password  => ENV["SENDGRID_PASSWORD"], 
-      :domain => "heroku.com",
+      :domain => "herokuapp.com",
       :address => "smtp.sendgrid.net",
       :port => 587,
       :authentication => :plain,
