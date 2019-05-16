5b02cc96888fdeea6be11e655778b69a1dcb86da
Kemomoy <surumekarasu@gmail.com>
2019-05-16 18:36:44 +0000

    add title,dircription column to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_06_042749) do
+ActiveRecord::Schema.define(version: 2019_05_16_151217) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
