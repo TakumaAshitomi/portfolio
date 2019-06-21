34b55e036d94f5a4f27de8f171c2865aff5e1927
TakumaAshitomi <surumekarasu@gmail.com>
2019-06-21 07:37:09 +0000

    create view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def show
     @user  = User.find(params[:id])
     @page_id = params[:id]
-    @activities = PublicActivity::Activity.all
+    @activities = PublicActivity::Activity.all.order(created_at: :desc)
   end
   
   def index
