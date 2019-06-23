bfc10514ca6aaa8fd6ecec9ebc06f6294385488a
TakumaAshitomi <surumekarasu@gmail.com>
2019-06-23 23:58:04 +0000

    create subs model varidation test

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :subscription do
-    
+    user_id {1}
+    sequence(:followed_id) { |n| "#{n}"}
   end
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before do
+    5.times do
+    create(:user)
+    end
+    @subs = build(:subscription)
+  end
+  
+  it "is invalid without a user_id" do
+    @subs.user_id = nil
+    expect(@subs).to_not be_valid
+  end
+
+  it "is invalid without a followed_id" do
+    @subs.followed_id = nil
+    expect(@subs).to_not be_valid
+  end
+  
+  it "is valid normal number" do
+    expect(@subs).to be_valid
+  end
+
+  context "user has two subs" do
+    
+    before do 
+      @subs2 = build(:subscription)
+    end
+    
+    it "disallow same subs" do
+      @subs2.followed_id = @subs.followed_id
+      @subs.save
+      expect(@subs2).to_not be_valid
+    end 
+    
+    it "allow another followed_id" do
+      expect(@subs2).to be_valid
+    end
+  end
 end
