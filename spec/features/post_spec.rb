2a8a2a2bd823a52a06bbe4c49e97344ba42dcd14
TakumaAshitomi <surumekarasu@gmail.com>
2019-07-06 05:00:26 +0000

    create edit scenario

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -11,4 +11,17 @@ RSpec.feature "Post", type: :feature do
     click_button "登録する"
     expect(page).to have_content "作成しました。"
   end
+  
+  scenario "can edit by owner" do
+    @user = create(:user)
+    @user.confirm
+    login_as(@user)
+    @post = create(:post)
+    @post.user_id = @user.id
+    visit "/posts/#{@post.id}/edit"
+    fill_in "title", with: "testtitle2"
+    fill_in "editor", with: "testdescription2"
+    click_button "変更"
+    expect(page).to have_content "変更しました。"
+  end
 end
