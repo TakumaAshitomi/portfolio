885ddec144c8b9233a64b492e9dc5bd87752c739
TakumaAshitomi <surumekarasu@gmail.com>
2019-07-17 15:08:58 +0000

    Add before_action for manage page

--- app/controllers/management_controller.rb
+++ app/controllers/management_controller.rb
@@ -1,8 +1,18 @@
 class ManagementController < ApplicationController
+  before_action :admin_user_only
+  
   def manage
     @userquery = User.ransack(params[:q])
     @users = @userquery.result.page(params[:page]).per(16)
     @postquery = Post.ransack(params[:q])
     @posts = @postquery.result.page(params[:page]).per(16)
   end
+
+  private
+    def admin_user_only
+      redirect_to root_url unless current_user.admin?
+      flash[:notice] = "管理ユーザー専用ページです。"
+    end
 end
+
+
