b37989c7cd8497f122e0898f62dc9dc085e888a1
kemomoy <surumekarasu@gmail.com>
2019-09-02 19:54:09 +0900

    Improve test

--- spec/system/post_spec.rb
+++ spec/system/post_spec.rb
@@ -24,22 +24,6 @@ RSpec.feature "Post", type: :system do
     expect(page).to have_content "変更しました。"
   end
 
-  # scenario "Can delete by owner", js: true do
-  #   user = create(:user)
-  #   login(user)
-  #   visit posts_new_path
-  #   fill_in "title", with: "testtitle"
-  #   fill_in_ckeditor "editor", with: "Some text"
-  #   execute_script("window.scrollBy(0,1000)")
-  #   click_on "登録する"
-  #   visit "/users/#{user.id}/"
-  #   click_on "投稿管理"
-  #   click_on "編集"
-  #   click_on "削除"
-  #   page.driver.browser.switch_to.alert.accept
-  #   expect(page).to have_content "削除しました"
-  # end
-
   scenario "Can see by followed user" do
     user = create(:user)
     user2 = create(:user)
@@ -93,4 +77,5 @@ RSpec.feature "Post", type: :system do
     expect(page).to have_content "tagA"
     expect(page).to_not have_content "tagB"
   end
+
 end
