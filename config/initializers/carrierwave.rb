c9724e25de191f2a32622265b96dd589807d88b0
kemomoy <surumekarasu@gmail.com>
2020-02-29 01:00:43 +0900

    Add for deploy by capistrano

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ CarrierWave.configure do |config|
     config.fog_directory  = 'kemomoi'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],,
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       region: "ap-southeast-1",
       path_style: true
