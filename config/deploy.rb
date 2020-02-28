7c9effbbd25c150340af2628a62a23356754abcc
kemomoy <surumekarasu@gmail.com>
2020-02-29 02:05:55 +0900

    Add for deploy by capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,8 @@ set :branch, "master"
 set :deploy_to, "/var/www/rails/portfolio"
 
 # シンボリックリンクをはるファイル。(※後述)
-set :linked_files, "config/master.key"
+append :linked_files, "config/master.key"
+
 # シンボリックリンクをはるフォルダ。(※後述)
 set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system")
 
