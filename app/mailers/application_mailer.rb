56f7817c4f1e9aa225159f597c229e5c6263b592
kemomoy <surumekarasu@gmail.com>
2020-03-28 01:30:02 +0900

    Change files for ses

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "Noreply <noreply@coaching-web-app.ml>"
   layout "mailer"
 end
