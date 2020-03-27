d75c821e7700458e3dafea2f8adebe965687f166
kemomoy <surumekarasu@gmail.com>
2020-03-28 01:56:11 +0900

    Change files for ses

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,10 +13,10 @@ Rails.application.configure do
   config.log_tags = [:request_id]
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = {
-    host: "http://coaching-web-app.ml/"
+    host: "coaching-web-app.ml"
   }
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :ses
   config.action_mailer.smtp_settings = {
     user_name: Rails.application.credentials.ses[:access_key_id],
     password: Rails.application.credentials.ses[:secret_access_key],
