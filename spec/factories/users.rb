9059f1ed76c006664024f00328c4b5155d300295
kemomoy <surumekarasu@gmail.com>
2020-09-07 03:14:43 +0900

    Fix typo

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     name { "testuser" }
     sequence(:email) { |n| "example#{n}@mail.com" }
     password { "passWo!rd21" }
-    password_confirmation { "passWo!rd21"}
+    password_confirmation { "passWo!rd21" }
     role { "member" }
   end
   factory :adminuser, class: User do
